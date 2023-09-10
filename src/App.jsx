d030c7bbd0a1eb4794e95dc4fee03b7a62889a7d
Mujahid2000 <developermujahid2001@gmail.com>
2023-09-10 19:38:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import Counter from './Counter'
+import Friends from './Friends'
+import Team from './Team'
+import Users from './Users'
+
+
+
+function App() {
+
+  function handleClick(){
+    alert('button Clicked')
+  }
+
+  const handleClicked2 = () => {
+    alert('button 2 Clicked')
+  }
+  
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    
+    <>
+      <h3>React Core Concept 2</h3>
+      <Friends></Friends>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
+      {/* <button onClick={handleClick}>Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClicked2}>Click Me</button>
+      <button onClick={() => {alert('third Clicked')}}>Third</button>
+      <button onClick={() => addToFive(3)}>Four</button>
+      
+    </>
+  )
+}
+
+export default App
